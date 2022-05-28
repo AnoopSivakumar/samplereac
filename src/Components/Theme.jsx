18cbf8d13da052dd0c73cc1d6a286bfb5e895bcd
MagicCoverzz <magiccoverzz@gmail.com>
2022-05-28 09:50:27 +0530

    first commit

---
+++ src/Components/Theme.jsx
@@ -0,0 +1,8 @@
+export const Theme = {
+  colors: {
+    base1: "#009ef2", // deep green backgrounf [navbar, about , contact]
+    base2: "#fff", //white
+    primary: "#009ef2", //lighter green
+    primary1: "#0085cc", // deepest green [portfolio, footer]
+  },
+};
